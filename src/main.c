17bb5c43c45f4d81527942c2209be87af0fce40d
iibrahimli <imranibrahimli98@gmail.com>
2019-03-07 09:48:39 +0400

    add argument parsing

--- src/main.c
+++ src/main.c
@@ -12,16 +12,36 @@
 
 
 // default values for parameters
+#define DEF_TIMEFMT  NULL
 #define DEF_INTERVAL 10000
 #define DEF_LIMIT    0
-#define DEF_CHKINT   0
-#define DEF_TIMEFMT  NULL
+#define DEF_CHKEXIT  0
 
 
 
-int main(int argc, char const *argv[]){
+int main(int argc, char *argv[]){
 
-    usage();
+    char *     timefmt  = DEF_TIMEFMT;
+    useconds_t interval = DEF_INTERVAL * 1000;
+    int        limit    = DEF_LIMIT;
+    int        chkexit  = DEF_CHKEXIT;
+
+
+    int option = 0;
+    while((option = getopt(argc, argv, "+t:i:l:c")) != -1){
+        switch (option){
+            case 't':
+                printf("-t: %s\n", optarg); break;
+            case 'i':
+                printf("-i: %s\n", optarg); break;
+            case 'l':
+                printf("-l: %s\n", optarg); break;
+            case 'c':
+                printf("-c: set\n"); break;
+            case '?':
+                fatal_error("Invalid option"); break;
+        }
+    }
 
     return 0;
 }
